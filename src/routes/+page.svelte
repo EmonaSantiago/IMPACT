63fc90dafad8ddddf64727165b87d7198ae34cf4
Emona Santiago <emonasantiago1207@gmail.com>
2024-03-29 15:32:26 +0100

    Navigatie route pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
+<!-- NAVIGATIE -->
+<nav>
+    <a href="/TIMELINE">TIMELINE</a> |
+    <a href="/CREATIVES">CREATIVES</a> |
+    <a href="/ABOUT-US">ABOUT US</a> |
+    <a href="/CONTACT">CONTACT</a>
+</nav>
+
+<!-- BODY -->
 <h1>IMPACT APPLICATIE INCOMING . . .</h1>
+
+<style>
+    a {
+        color: var(--IMPACT-yellow);
+        text-decoration: none;
+    }
+
+    h1 {
+        color: var(--IMPACT-white);
+    }
+</style>
